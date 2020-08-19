06778c2ee2d3c4c94e590bebf2e6748ad99ecca2
roronoa2112zoro <sahilmunshi98@gmail.com>
2020-08-19 15:46:21 +0530

    initial commit

---
+++ build/precache-manifest.ff684c65f02a874ebfb2e8ef13b298f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b903e0c0f806b0faad97580a55db4309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a74ea5d96c45bcbfe5",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "86a7ce61ffef877c5f84",
+    "url": "/static/js/2.291e4eb9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.291e4eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a74ea5d96c45bcbfe5",
+    "url": "/static/js/main.2cc251be.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "74bb39b591154f315713ba92c426ee25",
+    "url": "/static/media/News.74bb39b5.jpg"
+  }
+]);
